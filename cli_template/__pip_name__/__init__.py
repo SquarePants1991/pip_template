73ad431ea96537fa8fcce3789f8bbfc2b0aa9e20
yang wang <yangwang@yangdeMacBook-Pro.local>
2019-01-08 18:10:01 +0800

    init

---
+++ cli_template/__pip_name__/__init__.py
@@ -0,0 +1,2 @@
+def hello_pip():
+    print('hello pip!')
---
+++ cli_template/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='__pip_name__',
+      version='0.1',
+      description='__pip_description__',
+      url='__pip_url__',
+      author='__author__',
+      author_email='__author_email__',
+      license='MIT',
+      packages=setuptools.find_packages(),
+      zip_safe=False)
